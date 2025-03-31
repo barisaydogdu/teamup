4f9c5f0c0c5e62dcd44cd18f099b247eba9d994e
barisaydogdu <barisaydogdu3@gmail.com>
2025-03-31 21:14:17 +0300

    Add: user model added

--- src/main/java/com/filepackage/entity/Users.java
+++ src/main/java/com/filepackage/entity/Users.java
@@ -1,4 +1,99 @@
 package com.filepackage.entity;
 
-public class Users {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users",schema = "public", uniqueConstraints = {@UniqueConstraint(columnNames = "email")})
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users implements UserDetails {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "profile_picture")
+    private String profile_picture;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "phone_number")
+    private String phone_number;
+
+    @Column(name = "phone_visibility")
+    private String phone_visibility;
+
+    @Column(name = "account_created_at")
+    private LocalDateTime account_created_at;
+
+    @Column(name = "last_login")
+    private LocalDateTime last_login;
+
+    @Column(name = "account_status")
+    private String account_status;
+
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
